2aa8329274ef605c1e7c5b47d87342144cebc420
Ankur Das <66483907+AnkurDas-krypto@users.noreply.github.com>
2023-04-15 23:14:38 -0400

    Updated code to detect both log and throw files

--- src/main/java/ca/concordia/soen6591/antipattern_detector/visitors/LogAndThrowDetector.java
+++ src/main/java/ca/concordia/soen6591/antipattern_detector/visitors/LogAndThrowDetector.java
@@ -16,7 +16,6 @@ import ca.concordia.soen6591.antipattern_detector.detecters.StaticAnalyzer;
 public class LogAndThrowDetector extends ASTVisitor {
 
     private int numAntiPatternsDetected = 0;
-    private int numCatchClauses = 0;
     private String compilationUnitName;
     private CompilationUnit compilationUnit;
     private List<LogDetection> detectionList = new ArrayList<>();
@@ -32,38 +31,54 @@ public class LogAndThrowDetector extends ASTVisitor {
     * @return true if the visitor should continue visiting the AST, false otherwise
     */
 
+
+    
+    
     @SuppressWarnings("unchecked") 
     @Override
     public boolean visit(CatchClause node) {
-        numCatchClauses++;
         List<Statement> statements = node.getBody().statements();
+        boolean logDetected = false;
+        boolean throwDetected = false;
         for (Statement statement : statements) {
             if (statement.getNodeType() == Statement.EXPRESSION_STATEMENT) {
                 MethodInvocation invocation = (MethodInvocation) ((org.eclipse.jdt.core.dom.ExpressionStatement) statement).getExpression();
                 if (invocation.getName().toString().equals("printStackTrace")) {
                     int lineNumber = compilationUnit.getLineNumber(invocation.getStartPosition());
                     detectionList.add(new LogDetection(compilationUnitName, lineNumber));
-                    
-                    //rethrow
-                    AST ast = node.getAST();
-                    ThrowStatement throwStatement = ast.newThrowStatement();
-                    throwStatement.setExpression(ast.newSimpleName(node.getException().getName().getIdentifier()));
-                    node.getBody().statements().add(throwStatement);
-                    
+                    logDetected = true;
                     numAntiPatternsDetected++;
                 }
+            } else if (statement.getNodeType() == Statement.THROW_STATEMENT) {
+                int lineNumber = compilationUnit.getLineNumber(statement.getStartPosition());
+                detectionList.add(new LogDetection(compilationUnitName, lineNumber));
+                throwDetected = true;
+                numAntiPatternsDetected++;
+                
             }
+        }
+        //It replaces the log statement with a throw statement that rethrows the caught exception. 
+        //This way, the exception is being properly handled and thrown again to the calling method for further handling or reporting.
+        if (logDetected && throwDetected) {
+            // Replace the log with a throw statement
+            AST ast = node.getAST();
+            ThrowStatement throwStatement = ast.newThrowStatement();
+            throwStatement.setExpression(ast.newSimpleName(node.getException().getName().getIdentifier()));
+            node.getBody().statements().clear();
+            node.getBody().statements().add(throwStatement);
+            
         }
         return super.visit(node);
     }
+    
+    
+
+
 
     public int getNumAntiPatternsDetected() {
         return numAntiPatternsDetected;
     }
 
-    public int getNumCatchClauses() {
-        return numCatchClauses;
-    }
 
     public List<LogDetection> getDetectionList() {
         return detectionList;
@@ -88,5 +103,7 @@ public class LogAndThrowDetector extends ASTVisitor {
     }
     
 }
+
+
 
 
